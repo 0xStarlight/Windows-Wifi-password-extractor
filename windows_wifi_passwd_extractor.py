43a42fe2e3ed04085f6e7062d33bd28e7ac60496
Bhaskar Pal <bhaskarpal347@gmail.com>
2021-10-22 14:37:11 +0530

    Update windows_wifi_passwd_extractor.py

--- windows_wifi_passwd_extractor.py
+++ windows_wifi_passwd_extractor.py
@@ -6,19 +6,31 @@ lines = data.splitlines()
 
 SSID_names = []
 
+
 for i in lines:
     SSID_names.append(i[i.find(":")+3 :-1])
 
+
 data2 = ''
 for i in range(len(lines)):
-    ret_pass = os.popen(f'netsh wlan show profiles name={SSID_names[i]} key=clear | findstr "Key Content"')
-    data2 += ret_pass.read()
+    ret_pass = os.popen(f'netsh wlan show profiles name="{SSID_names[i]}" key=clear | findstr "Key Content"')
+    
+    check = ret_pass.read()
+    if len(check) == 0:
+        data2 += '\n'
+    if len(check) == 31:
+        data2+= '\n'
+    else:
+        data2 += check
+    
 lines2 = data2.splitlines()
 
 SSID_pass = []
 
+
 for i in lines2:
-    SSID_pass.append(i[i.find(":")+2 :-1])
+    SSID_pass.append(i[i.find(":")+2 ::])
+
 
 table = BeautifulTable()
 table.columns.header = ["SSID Username", "SSID Password"]
